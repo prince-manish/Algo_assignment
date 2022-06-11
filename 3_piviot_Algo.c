5d2508ac50400a0617eeab51d7451c6af800222c
Manish chauhan <52108318+prince-manish@users.noreply.github.com>
2022-06-11 19:00:52 +0530

    Update 3_piviot_Algo.c

--- 3_piviot_Algo.c
+++ 3_piviot_Algo.c
@@ -1,56 +1,54 @@
-void quicksort3PivotBasic(int A[], int lo, int hi) {
+void quicksort3Pivot(int A[], int lo, int hi) {
     int length = hi - lo + 1;
     int midpoint;
-    int p,q,r;
-    int a,b,c,d;
+    int x, y, z;
+    int a, b, c, d;
     int t;
-    if(hi-lo < 1 || lo<0 || hi<0 )
+    if (hi - lo < 1 || lo < 0 || hi < 0)
         return;
-    midpoint = (lo + hi) /2;
-    if (A[midpoint] < A[lo])
-    {
+    midpoint = (lo + hi) / 2;
+    if (A[midpoint] < A[lo]) {
         t = A[midpoint];
         A[midpoint] = A[lo];
         A[lo] = t;
     }
-    if (A[hi] < A[midpoint])
-    {
+    if (A[hi] < A[midpoint]) {
         t = A[hi];
         A[hi] = A[midpoint];
         A[midpoint] = t;
-        if (t < A[lo])
-        {
+        if (t < A[lo]) {
             A[midpoint] = A[lo];
-            A[lo] = t; }
+            A[lo] = t;
+        }
     }
-    p = A[lo];
-    q = A[midpoint];
-    r = A[hi];
-    swap(A, lo+1, midpoint);
+    x = A[lo];
+    y = A[midpoint];
+    z = A[hi];
+    swap(A, lo + 1, midpoint);
     a = lo + 2;
     b = lo + 2;
     c = hi - 1;
     d = hi - 1;
-    while (b <= c)
-    {
-        //printf("\n--- WHILE START");
-        //print(A,n);
-        while (A[b] < p && b <= c)
-        {
-            if (A[b] < r) {
+    while (b <= c) {
+        while (A[b] < y && b <= c) {
+            if (A[b] < x) {
                 swap(A, a, b);
-                a++; }
-            b++; }
-        while (A[c] > q && b <= c)
-        {
-            if (A[c] > r) {
+                a++;
+            }
+            b++;
+        }
+        while (A[c] > y && b <= c) {
+            if (A[c] > z) {
                 swap(A, c, d);
-                d--; }
-            c--; }
+                d--;
+            }
+            c--;
+        }
         if (b <= c) {
-            if (A[b] > r) {
-                if (A[c] < p) {
-                    swap(A, b, a); swap(A, a, c);
+            if (A[b] > z) {
+                if (A[c] < x) {
+                    swap(A, b, a);
+                    swap(A, a, c);
                     a++;
                 } else {
                     swap(A, b, c);
@@ -60,24 +58,29 @@ void quicksort3PivotBasic(int A[], int lo, int hi) {
                 c--;
                 d--;
             } else {
-                if (A[c] < p) {
-                    swap(A, b, a); swap(A, a, c);
+                if (A[c] < x) {
+                    swap(A, b, a);
+                    swap(A, a, c);
                     a++;
                 } else {
                     swap(A, b, c);
                 }
-                b++; c--; }
+                b++;
+                c--;
+            }
         }
-//  printf("\n--- WHILE END");
-//  print(A,n);
     }
-    a--; b--; c++; d++;
-    swap(A, lo + 1, a); swap(A, a, b);
+    a--;
+    b--;
+    c++;
+    d++;
+    swap(A, lo + 1, a);
+    swap(A, a, b);
     a--;
     swap(A, lo, a);
     swap(A, hi, d);
-    quicksort3PivotBasic(A, lo, a-1);
-    quicksort3PivotBasic(A, a+1, b-1);
-    quicksort3PivotBasic(A, b+1, d-1);
-    quicksort3PivotBasic(A, d+1, hi);
+    quicksort3Pivot(A, lo, a - 1);
+    quicksort3Pivot(A, a + 1, b - 1);
+    quicksort3Pivot(A, b + 1, d - 1);
+    quicksort3Pivot(A, d + 1, hi);
 }
